104d41aa4610ff926ec64a13e29ef329d25591b9
luismonzon <luismonzon@outlook.com>
2017-10-27 10:21:43 -0600

    Promises y services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -66,14 +55,7 @@ const HEROES: Hero[] = [
   }
 `],
   template:`<h1>{{title}}</h1>
-  <div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-  </div>
-</div>
+  <hero-detail [hero]="selectedHero"></hero-detail>
 <h2>My Heroes</h2>
 <ul class="heroes">
   <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
@@ -81,22 +63,32 @@ const HEROES: Hero[] = [
   </li>
 </ul>
 `
+,
+ providers: [HeroService]
 
 
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  heroes= HEROES;
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
-}
+  heroes: Hero[];
 
 
-export class Hero{
-  id: number;
-  name: string;
+
+   constructor(private heroService: HeroService) { }
+
+    onSelect(hero: Hero): void{
+      this.selectedHero = hero;
+    }
+    getHeroes(): void {
+    this.heroService.getHeroes().then(heroes=> this.heroes=heroes);
+    }
+    ngOnInit(): void{
+      this.getHeroes();
+
+    }
+
 }
